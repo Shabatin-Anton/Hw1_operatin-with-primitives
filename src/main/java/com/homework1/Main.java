e6920e65e0a1b7db58c9d6c847f14b4661d14aad
Shabatin-Anton <antonshabatin1993@gmail.com>
2020-06-18 20:07:11 +0300

    Initial commit

---
+++ src/main/java/com/homework1/Main.java
@@ -0,0 +1,16 @@
+package main.java.com.homework1;
+
+public class Main {
+    public static void main(String[] args) {
+        byte a = 12;
+        short d = 1000;
+        long g = 4358214;
+        float pi = 3.14f;
+        double x = 34.2357;
+        System.out.println(a + d);
+        System.out.println(pi / x);
+        System.out.println(d - a);
+        System.out.println(g * a);
+
+    }
+}
